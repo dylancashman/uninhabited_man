f039fc66001744b49d29cb228db5c03faee3bcdb
Dylan Cashman <dylan.cashman@annkissam.com>
2015-04-10 15:34:47 -0400

    * Customize CSS and fonts

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,5 +1,5 @@
 module PostsHelper
   def markdown_renderer
-    @markdown_renderer ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
+    @markdown_renderer ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(prettify: true), autolink: true, tables: true)
   end
 end
