6554fd4a42f03abc555a297f7aa0642617852576
Dylan Cashman <dylan.cashman@annkissam.com>
2015-04-17 14:55:10 -0400

    Additional fixups to site iterations page

--- app/models/site_iteration.rb
+++ app/models/site_iteration.rb
@@ -1,4 +1,5 @@
 class SiteIteration < ActiveRecord::Base
+  belongs_to :referential_post, class_name: 'Post'
   has_many :posts
   has_attached_file :screenshot, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment :screenshot, presence: true, # weird paperclip syntax
